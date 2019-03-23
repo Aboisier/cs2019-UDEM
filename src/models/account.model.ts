b57df4120f0bcb8b7f2f23a295f9cf1db6e0a519
mathieu <math.per@hotmail.com>
2019-03-23 11:08:31 -0400

    constructeur

--- src/models/account.model.ts
+++ src/models/account.model.ts
@@ -1,9 +1,24 @@
 export class Account {
+
+
+    userId: String;
+    email: String;
+    passwordHash: String;
+    passordSalt: String;
+    fullName:String;
+    
     constructor(
         public userId: string,
         public email: string,
         public passwordHash: string,
         public passwordSalt: string,
         public fullName: string
-    ) { }
+    ) {
+        this.userId = user;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.passwordSalt = passwordSalt;
+        this.fullName = fullName;
+    }
+    
 }

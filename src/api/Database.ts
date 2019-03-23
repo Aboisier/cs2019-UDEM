256e259cd3cd8362773484fff919e36f64e7e4a4
mathieu <math.per@hotmail.com>
2019-03-23 16:05:47 -0400

    added the insertions in the database and creation on methods to authentificate

--- src/api/Database.ts
+++ src/api/Database.ts
@@ -7,12 +7,27 @@ export class Database {
     private static database : Database = new Database();
 
     public static getQueryResult(callback){
-        MongoClient.connect( process.env.MONGODB_URI , function (err, client) {
+        Database.connect( function (err, client) {
             
             callback(err,client);
             
             client.close();
             
         });
-    }   
+    }
+
+    public static insert(collectionName,insertSubject){
+        
+        Database.connect( function(err, client) {
+            // Get the collection
+            var col = client.db.collection(collectionName);
+            col.insertOne(insertSubject);
+            client.close();
+        });
+    }
+
+    private static connect(callback){
+        MongoClient.connect( process.env.MONGODB_URI ,callback);
+
+    }
 }
--- src/api/api.ts
+++ src/api/api.ts
@@ -3,6 +3,7 @@ import { sign } from 'jsonwebtoken';
 import { Database } from './Database';
 
 export class Api {
+
     
     public getRouter(): Router {
         
@@ -10,40 +11,41 @@ export class Api {
         // TODO: You probably want to register your routes here.
         router.post('/auth/createAccount', this.createAccount);
         router.post('/api/auth/authenticate', this.authenticate);
-                    
+        
         return router;
     }
 
     // cree l'account a partir des elements envoyes par l'utilisateur
     private createAccount(req,rep){
 
-        var email: string = req.body.email;
-        var password: string = req.body.password;
-        var fullName: string = req.body.fullName;
+        var mail: string = req.body.email;
+        var pass: string = req.body.password;
+        var fullNameX: string = req.body.fullName;
 
-        if ( email != undefined && password != undefined && fullName != undefined ) {
+        if ( mail != undefined && pass != undefined && fullNameX != undefined ) {
             rep.sendStatus(400)
         }
-        //TODO faire la fonction accountExist
-        else if ( this.accountExist(email) ){
+        
+        else if ( this.accountExist( mail) ){
             rep.sendStatus(500);
         }
         else {
+            Database.insert('users',{email:mail,password:pass,fullName:fullNameX})
             rep.sendStatus(201);
         }
     }
 
-    private accountExist( email: string){
-    
+    private accountExist( emailUser: string){
+        
         var isThere = false;
         
         Database.getQueryResult( function (err, client) {
             if (err){
                 throw err
             }
-        
             
-            client.db.collection('usernames').find().toArray(function (err, result) {
+            
+            client.db.collection('users').find({email:emailUser}).toArray(function (err, result) {
                 if (err){
                     throw err;
                 }
@@ -62,10 +64,23 @@ export class Api {
         
     }
 
-    private authenticate(){
-        //res.json(json
-    }
-    
-    
+    // we have to verify if the account is in the database and if so compare his pswd
+    // with the pswd from the database
+    private authenticate(emailUser,passUser){
+        var correspondance;
+        Database.getQueryResult( function (err, client) {
+            if (err){
+                throw err
+            }
+            correspondance =  client.db.collection('users').find({email:emailUser,password:passUser}).count()!=0;
+        });
+
+        if(correspondance){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }    
 }
-    
+

e14096e7c5a4ab36c6dd36119d10325ef8d737e6
mathieu <math.per@hotmail.com>
2019-03-23 11:57:45 -0400

    functions for the router, but still not working. 2 methods more to implements.

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,37 @@
 import { Router } from 'express';
 
 export class Api {
+    
     public getRouter(): Router {
         const router = Router();
         // TODO: You probably want to register your routes here.
+        router.post('/auth/createAccount', createAccount)
+
         return router;
     }
+
+    // fonction qui cree l'account a partir des elements envoyes par l'utilisateur
+    private function createAccount(req,rep){
+
+        var email: string = req.body.email;
+        var password: string = req.body.password;
+        var fullName: string = req.body.fullName;
+
+        if ( email != undefined && password != undefined && fullName != undefined ) {
+            rep.sendStatus(400)
+        }
+        //TODO faire la fonction accountExist
+        else if ( accountExist(email) ){
+            rep.sendStatus(500);
+        }
+        else {
+            rep.sendStatus(201);
+        }
+    }
+
+    private function accountExist(String email){
+
+        
+    }
 }
+
